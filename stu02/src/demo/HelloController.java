7775cce591f4824e11edb3bcd6f0236f44f1ab14
xiao <472036660@qq.com>
2019-12-10 15:26:05 +0800

    2019年12月10日15:26:03

---
+++ stu02/src/demo/HelloController.java
@@ -0,0 +1,55 @@
+package demo;
+import com.jfinal.core.Controller;
+public class HelloController extends Controller {
+    public void index() {
+       renderText("Hello JFinal World.");
+    }
+   public void mongodbtest() {
+	   render("querylist.html");
+   }
+   /**
+    * 打开登录页面
+    */
+   public void openlogin() {
+   	render("login.html");
+   }
+   /**
+    * 登录
+    * 1、判断用户名
+    * 2、判断密码
+    */
+   public void login() {
+	   //1、获取页面用户名和密码
+	   String username=getPara("username");
+	   String password=getPara("password");
+	   //2、从mongodb数据库获取用户名和密码
+	   String user="201702";
+	   String pw="123";
+	   //3、判断用户名是否存在
+	   if(username.equals(user)) {//存在用户名
+		   //判断密码
+		   if(password.equals(pw)) {//密码正确
+			   //进入主页面
+			   setAttr("result", 0);
+			   renderJson();
+		   }else {
+			   //提示密码错误，重新输入密码
+			   setAttr("result", 1);
+			   renderJson();
+		   }
+		   
+	   }else {//不存在用户名
+		   //提示用户名不存在，重新输入用户名和密码
+		   setAttr("result", -1);
+		   renderJson();
+	   }
+	   
+   }
+   /**
+    * 打开主页面
+    */
+   public void openmain() {
+	   render("main.html");
+   }
+   
+}
